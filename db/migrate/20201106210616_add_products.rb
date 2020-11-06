3f2a168e22f0cbae97d5d876b05eb3e0f1717e9b
KotSergiy <ks77vl@gmail.com>
2020-11-06 23:53:38 +0200

    Create migrations Products and AddProducts

---
+++ db/migrate/20201106210616_add_products.rb
@@ -0,0 +1,34 @@
+class AddProducts < ActiveRecord::Migration[6.0]
+  def change
+    Product.create(
+      :title=>'Гавайская',
+      :description=>'Это Гавайская пицца.',
+      :price=>350,
+      :size=>30,
+      :is_spicy=>false,
+      :is_veg=>false,
+      :is_best_offer=>false,
+      :path_to_image=>'/images/hawai.jpg'
+    )
+    Product.create(
+      :title=>'Пеперонни',
+      :description=>'Это острая пицца.',
+      :price=>450,
+      :size=>30,
+      :is_spicy=>false,
+      :is_veg=>false,
+      :is_best_offer=>false,
+      :path_to_image=>'/images/pepperoni.jpg'
+    )
+    Product.create(
+      :title=>'Овощная',
+      :description=>'Это вегатарианская пицца.',
+      :price=>400,
+      :size=>30,
+      :is_spicy=>false,
+      :is_veg=>true,
+      :is_best_offer=>false,
+      :path_to_image=>'/images/veg.jpg'
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_211724) do
+ActiveRecord::Schema.define(version: 2020_11_06_210616) do
 
   create_table "Products", force: :cascade do |t|
     t.string "title"
